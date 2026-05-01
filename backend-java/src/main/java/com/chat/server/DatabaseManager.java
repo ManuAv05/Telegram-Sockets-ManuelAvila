60bcdd1a3ab654c05850c514b2fc0992cf4d5228
Manuel Avila Dugo <manuelaviladugo@gmail.com>
2026-05-01 13:56:34 +0200

    arreglos

--- backend-java/src/main/java/com/chat/server/DatabaseManager.java
+++ backend-java/src/main/java/com/chat/server/DatabaseManager.java
@@ -3,6 +3,7 @@ package com.chat.server;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.mindrot.jbcrypt.BCrypt;
